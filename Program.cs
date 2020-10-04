19bd1f9b7e2b2323fd84437b6a3eca3bd6c49911
Satantra Tewari <satantra1508@gmail.com>
2020-10-04 23:13:36 +0530

    [satantra] options checked for noplay, ladder or snake

--- Program.cs
+++ Program.cs
@@ -6,16 +6,35 @@ namespace SnakeLadderGame
     class Program
     {
         public const int START_POSITION = 0, END_POSITION = 100;
+        public const int NO_PLAY = 0, LADDER = 1, SNAKE = 2;
         public static Random random = new Random();
         public static int DiceRoll()
         {
             return random.Next(1, 7);
+        }
+        public static int Movement(int diceValue)
+        {
+            int checkOption = random.Next(0, 3);
+            int stepsMoved = 0;
+            switch (checkOption)
+            {
+                case LADDER:
+                    stepsMoved = diceValue;
+                    break;
+                case SNAKE:
+                    stepsMoved = -diceValue;
+                    break;
+                default:
+                    break;
+            }
+            return stepsMoved;
         }
         static void Main(string[] args)
         {
             Console.WriteLine(" Welcome to Online Snake and Ladder Game");
             int currentPosition = START_POSITION;
             int diceValue = DiceRoll();
+            int stepsToMove = Movement(diceValue);
         }
     }
 }

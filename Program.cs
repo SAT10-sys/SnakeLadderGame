cd53dd5b8db03a4015b25b1d2c89bb5ffe7efb2d
Satantra Tewari <satantra1508@gmail.com>
2020-10-04 23:11:31 +0530

    [satantra] the dice is rolled

--- Program.cs
+++ Program.cs
@@ -6,10 +6,16 @@ namespace SnakeLadderGame
     class Program
     {
         public const int START_POSITION = 0, END_POSITION = 100;
+        public static Random random = new Random();
+        public static int DiceRoll()
+        {
+            return random.Next(1, 7);
+        }
         static void Main(string[] args)
         {
             Console.WriteLine(" Welcome to Online Snake and Ladder Game");
             int currentPosition = START_POSITION;
+            int diceValue = DiceRoll();
         }
     }
 }

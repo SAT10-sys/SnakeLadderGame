9377282cc8e319aab91ca6be5238cf0105f47ff4
Satantra Tewari <satantra1508@gmail.com>
2020-10-04 23:07:18 +0530

    [satantra] welcome message displayed

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Snake_Ladder_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to Online Snake and Ladder Game ");
+        }
+    }
+}

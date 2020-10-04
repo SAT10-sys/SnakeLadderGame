692aa70103499e9aeb9d33e53061b100cb5c21dc
Satantra Tewari <satantra1508@gmail.com>
2020-10-04 23:09:31 +0530

    [satantra] game started at zero

--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
-namespace Snake_Ladder_Game
+namespace SnakeLadderGame
 {
     class Program
     {
+        public const int START_POSITION = 0, END_POSITION = 100;
         static void Main(string[] args)
         {
-            Console.WriteLine(" Welcome to Online Snake and Ladder Game ");
+            Console.WriteLine(" Welcome to Online Snake and Ladder Game");
+            int currentPosition = START_POSITION;
         }
     }
 }
